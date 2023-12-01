004bdab24f88a16210980ff236d9bc079cfec4c2
Kyle McBurnett <kylemcburnett@xingpeds.com>
2023-12-01 12:47:47 -0800

    update test from param changes

--- client-network/test-suite/src/commonTest/kotlin/Prototyping.kt
+++ client-network/test-suite/src/commonTest/kotlin/Prototyping.kt
@@ -1,11 +1,13 @@
 import com.xingpeds.kmirc.clientnetwork.randomSocketArb
 import io.kotest.property.arbitrary.next
+import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 
 class Prototyping {
     @Test
-    fun run() {
-        val hi = randomSocketArb()
+    fun run() = runTest {
+
+        val hi = randomSocketArb(this)
         hi.next().close()
     }
 }
