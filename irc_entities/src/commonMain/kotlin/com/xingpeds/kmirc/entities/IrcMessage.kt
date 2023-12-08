c2d8ce9d2a07238af332a7e5f51078c6772ce4a0
Kyle McBurnett <kylemcburnett@xingpeds.com>
2023-12-07 21:03:03 -0800

    better command enum

--- irc_entities/src/commonMain/kotlin/com/xingpeds/kmirc/entities/IrcMessage.kt
+++ irc_entities/src/commonMain/kotlin/com/xingpeds/kmirc/entities/IrcMessage.kt
@@ -14,19 +14,6 @@ data class IrcMessage(
     }
 }
 
-enum class IrcCommand {
-    PRIVMSG,  // Private message between users
-    NOTICE,   // Send notice to a user or a channel
-    JOIN,     // Join a channel
-    PART,     // Leave a channel
-    TOPIC,    // Change or view the topic of a channel
-    PING,     // Ping a server to test response
-    PONG,     // Reply to a ping message
-    NICK,     // Change nickname
-    MODE,     // Change channel or user modes
-    QUIT,     // Disconnect from the server
-}
-
 data class IrcPrefix(
     val nick: String,     // The nickname of the user
     val user: String?,    // The username of the user, optional
