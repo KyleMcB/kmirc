aa8ba404697dfe142bdf9e2f68096efab19c7a81
kyle mcburnett <kylemcburnett@xingpeds.com>
2023-12-20 16:26:44 -0800

    remove redundant copyright

--- irc_parser/implementation/src/commonTest/kotlin/ParserTest.kt
+++ irc_parser/implementation/src/commonTest/kotlin/ParserTest.kt
@@ -6,9 +6,6 @@ import com.xingpeds.kmirc.parser.IrcLineParser
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 
-/*
- * Copyright 2024 Kyle McBurnett
- */
 
 class ParserTest : ParserTestBase() {
     override fun getTestSubject(): IrcLineParser {
