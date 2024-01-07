d42b467ed604bb33a2c6b6b92cfea1fdbd710eab
Kyle McBurnett <kylemcburnett@xingpeds.com>
2024-01-06 17:55:04 -0800

    added interface for logging. I'll probably expand on it later.

---
+++ Utility_Functions/src/commonMain/kotlin/Logged.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2024 Kyle McBurnett
+ */
+
+/**
+ * interface for kermit logging. experimental
+ */
+interface Logged {
+    /**
+     * tag for kermit logging
+     */
+    val tag: String
+
+}
