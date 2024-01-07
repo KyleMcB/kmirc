b92caef5bec30e748d66a2d370fcb00733d72eee
Kyle McBurnett <kylemcburnett@xingpeds.com>
2024-01-06 19:31:22 -0800

    Add Kermit logger and create error logging function
    A new dependency to the Kermit logger (version 2.0.2) has been added to improve logging across the application. An error logging function 'withErrorLogging' has been also incorporated in Logged.kt. This function catches exceptions, logs informative details about them, and rethrows them for handling elsewhere, thereby enhancing debugging capabilities.

--- Utility_Functions/src/commonMain/kotlin/Logged.kt
+++ Utility_Functions/src/commonMain/kotlin/Logged.kt
@@ -2,6 +2,12 @@
  * Copyright 2024 Kyle McBurnett
  */
 
+import co.touchlab.kermit.Logger.Companion.e
+
+/*
+ * Copyright 2024 Kyle McBurnett
+ */
+
 /**
  * interface for kermit logging. experimental
  */
@@ -11,4 +17,28 @@ interface Logged {
      */
     val tag: String
 
+}
+
+/**
+ * basic error logging
+ */
+inline fun Logged.withErrorLogging(action: () -> Unit) {
+    try {
+        action()
+    } catch (e: Throwable) {
+        val trace = e.stackTrace.joinToString(separator = "\n") { stackTraceElement ->
+            stackTraceElement.toString()
+        }
+        val typename: String = e.javaClass.name
+        e(tag) {
+            """
+                wild caught exception
+                type: $typename
+                message: ${e.message}
+                cause: ${e.cause}
+            """.trimIndent()
+        }
+        e(tag) { "trace: $trace" }
+        throw e
+    }
 }
