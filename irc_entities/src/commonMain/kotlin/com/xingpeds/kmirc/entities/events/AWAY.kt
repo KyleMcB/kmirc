48ac01a6760096fdc2126100c453f81a58c17071
kyle mcburnett <kylemcburnett@xingpeds.com>
2024-01-10 18:01:30 -0800

    Introduced the AWAY event class
    Created a new AWAY class to represent an IRC event that handles user away status. The class logs an away message and timestamps each IRC event. It also throws an IllegalIRCMessage exception when the provided IRC message does not meet expectations.

---
+++ irc_entities/src/commonMain/kotlin/com/xingpeds/kmirc/entities/events/AWAY.kt
@@ -0,0 +1,32 @@
+package com.xingpeds.kmirc.entities.events
+
+import com.xingpeds.kmirc.entities.IrcCommand
+import com.xingpeds.kmirc.entities.IrcMessage
+import kotlinx.datetime.Instant
+import kotlin.jvm.Throws
+
+/**
+ * The `AWAY` class is used to represent an IRC event indicating that a user has set an away message.
+ * It implements the `IIrcEvent` interface.
+ *
+ * @param awayMessage The away message set by the user. If it is null or empty the user is no longer away
+ * @param timestamp The timestamp of the IRC event.
+ *
+ * @throws IllegalIRCMessage If the IRC message provided does not conform to the required format or is missing necessary information.
+ */
+class AWAY(val awayMessage:String?, override val timestamp: Instant) : IIrcEvent {
+    /**
+     * The `AWAY` class is used to represent an IRC event indicating that a user has set an away message.
+     * It implements the `IIrcEvent` interface.
+     *
+     * @property awayMessage The away message set by the user.
+     * @property timestamp The timestamp of the IRC event.
+     *
+     * @throws IllegalIRCMessage If the IRC message provided does not conform to the required format or is missing necessary information.
+     */
+    @Throws(IllegalIRCMessage::class)
+    constructor(ircMessage: IrcMessage) : this(
+        awayMessage = ircMessage.params.longParam,
+        timestamp = ircMessage.timestamp
+    )
+}
